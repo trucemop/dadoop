369fed7618245bffbfeec09f322f530d67a63b76
John Highcock <trucemop@gmail.com>
2014-06-15 12:03:56 -0400

    Better hadoop daemon calls.

--- multiname/start.sh
+++ multiname/start.sh
@@ -6,8 +6,10 @@ service dnsmasq start
 
 runuser -l hadoop -c 'hadoop namenode -format'
 
-runuser -l hadoop -c '/usr/lib/hadoop/bin/start-dfs.sh'
 
-runuser -l hadoop -c '/usr/lib/hadoop/bin/start-mapred.sh'
+runuser -l hadoop -c '/usr/lib/hadoop/bin/hadoop-daemon.sh --config /usr/lib/hadoop/conf start namenode'
+runuser -l hadoop -c '/usr/lib/hadoop/bin/hadoop-daemon.sh --config /usr/lib/hadoop/conf start datanode'
+runuser -l hadoop -c '/usr/lib/hadoop/bin/hadoop-daemon.sh --config /usr/lib/hadoop/conf start jobtracker'
+runuser -l hadoop -c '/usr/lib/hadoop/bin/hadoop-daemon.sh --config /usr/lib/hadoop/conf start tasktracker'
 
 /bin/sh
--- multinode/start.sh
+++ multinode/start.sh
@@ -4,9 +4,8 @@ service sshd start
 
 service dnsmasq start
 
-runuser -l hadoop -c '/usr/lib/hadoop/bin/hadoop-daemons.sh --config /usr/lib/hadoop/conf start datanode'
-
-runuser -l hadoop -c '/usr/lib/hadoop/bin/start-mapred.sh --config /usr/lib/hadoop/conf'
+runuser -l hadoop -c '/usr/lib/hadoop/bin/hadoop-daemon.sh --config /usr/lib/hadoop/conf start datanode'
+runuser -l hadoop -c '/usr/lib/hadoop/bin/hadoop-daemon.sh --config /usr/lib/hadoop/conf start tasktracker'
 
 /bin/sh
 
